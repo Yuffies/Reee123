3bbbbd6e955715e5df64768e752415075c858410
JAKUP-ACER\Jákup <jakha14@student.sdu.dk>
2017-05-01 16:22:25 +0200

    Ændret det igen IGEN THORPE

--- AndroidFrontend/app/src/main/java/android/reee123/MainActivity.java
+++ AndroidFrontend/app/src/main/java/android/reee123/MainActivity.java
@@ -3,26 +3,14 @@ package android.reee123;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Camera;
-import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.google.android.flexbox.FlexboxLayout;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,56 +18,35 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import static android.reee123.R.styleable.View;
-import static android.widget.GridLayout.spec;
-
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
-
+    private FlexboxLayout flexboxLayout;
     private int IMAGE_W = 0;
     private int IMAGE_H = 0;
-    private int arri = 0;
-    private int arrj = 0;
-    private int kat = 0;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-        //DisplayMetrics disp = new DisplayMetrics();
-        //getWindowManager().getDefaultDisplay().getMetrics(disp);
-
-        //IMAGE_H = IMAGE_W = disp.widthPixels/2;
-        System.out.println(IMAGE_H);
-
+        flexboxLayout = (FlexboxLayout) findViewById(R.id.images_grid);
+        flexboxLayout.setTop(0);
     }
 
-
     public void addImageToGrid(String imageURL) {
-
-
-        //GridLayout layout = (GridLayout) findViewById(R.id.images_grid);
-        LinearLayout layout = (LinearLayout) findViewById(R.id.images_grid);
-
         ImageView imageView = new ImageView(this);
         imageView.setMaxHeight(IMAGE_H);
         imageView.setMaxWidth(IMAGE_W);
         imageView.setMinimumHeight(IMAGE_H);
         imageView.setLayoutParams(new ViewGroup.LayoutParams(IMAGE_W, IMAGE_H));
-        URL url = null;
+        URL url;
         try {
             url = new URL(imageURL);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             InputStream is = connection.getInputStream();
             Bitmap img = BitmapFactory.decodeStream(is);
             imageView.setImageBitmap(img);
-            layout.addView(imageView);
-
+            flexboxLayout.addView(imageView);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -87,15 +54,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
     public void addImageToGrid(Bitmap imageURL) {
-        FlexboxLayout layout = (FlexboxLayout) findViewById(R.id.images_grid);
         ImageView imageView = new ImageView(this);
         imageView.setMinimumHeight(250);
         imageView.setMinimumWidth(250);
         imageView.setImageBitmap(imageURL);
-        layout.addView(imageView);
+        flexboxLayout.addView(imageView);
     }
 
     @Override
@@ -127,15 +91,7 @@ public class MainActivity extends AppCompatActivity {
                         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
                         }
-
-
                     }
-
-                    // Right to left swipe action
-                    else {
-                        System.out.println("rehehelllyyy");
-                    }
-
                 } else {
                     // consider as something else - a screen tap for example
                 }
@@ -143,11 +99,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onTouchEvent(event);
     }
-
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-
 }
